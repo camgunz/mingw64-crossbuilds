cd3dbecb96ad05b0abf79bfba150b741e733c488
Charlie Gunyon <charles.gunyon@gmail.com>
2014-11-25 15:24:03 +0000

    Work around problems with libpng's installation on Windows

--- modules-phase1/build-libpng-1.5.14.sh
+++ modules-phase1/build-libpng-1.5.14.sh
@@ -7,3 +7,10 @@
 
 make && make install
 
+# Work around libpng/libtool/MinGW-w64/Code::Blocks problem on Windows
+
+cp $BUILD_DIR/include/libpng15/* $BUILD_DIR/include/ || exit 1
+rm -f $BUILD_DIR/lib/libpng.* || exit 1
+cp $BUILD_DIR/lib/libpng15.a $BUILD_DIR/lib/libpng.a || exit 1
+cp $BUILD_DIR/lib/libpng15.dll.a $BUILD_DIR/lib/libpng.dll.a || exit 1
+

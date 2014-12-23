7969e32252d39efc92ca2554d78da32b341b02da
Charlie Gunyon <charles.gunyon@gmail.com>
2014-12-23 22:09:12 +0000

    Remove old module build scripts

--- modules/build-lua-5.2.3.sh
+++
@@ -1,8 +0,0 @@
-make AR="$BIN_DIR/$HOST_TRIPLET-ar rcu" \
-     RANLIB="$BIN_DIR/$HOST_TRIPLET-ranlib" \
-     STRIP="$BIN_DIR/$HOST_TRIPLET-strip" \
-     CC="$BIN_DIR/$HOST_TRIPLET-gcc" \
-     INSTALL_TOP="$BUILD_DIR" \
-     TO_BIN="lua.exe luac.exe lua52.dll" \
-     mingw install
-
--- modules/build-ragel-6.9.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./configure --build=$BUILD_TRIPLET \
-            --host=$HOST_TRIPLET \
-            --prefix=$PREFIX \
-            || exit 1
-
-make && make install
-
